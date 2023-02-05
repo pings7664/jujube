5fe9d96991de0750e39bcec395426b06aa1b23b4
gnip.gnos <abyss76647464@gmai.com>
2023-02-05 21:57:56 +0800

    :lipstick: nuxt add tailwindcss

--- apps/nuxt/nuxt.config.ts
+++ apps/nuxt/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  srcDir: "src/",
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
   modules: ['@nuxt/content']
 })
