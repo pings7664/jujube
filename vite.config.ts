7cf81bc2c66cc628ef1c6235bddfd87d1c67a5e2
gnip.gnos <abyss76647464@gmai.com>
2023-05-02 00:20:48 +0800

    :package: add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -7,6 +8,11 @@ import Layouts from 'vite-plugin-vue-layouts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${resolve(__dirname, 'src')}/`,
+    },
+  },
   plugins: [
     vue(),
     Pages(),
