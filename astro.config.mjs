3c67bb6699d72d84d4b68347f1b43fb68202d870
gnip.gnos <abyss76647464@gmai.com>
2023-10-02 12:35:11 +0800

    :lipstick: add tailwindcss and daisyui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
   adapter: vercel()
 });
