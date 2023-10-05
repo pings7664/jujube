66138a4f26ac7352f824b7b1093973c6bb615bc1
gnip.gnos <abyss76647464@gmai.com>
2023-10-05 13:33:16 +0800

    :wrench: update package and add icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,31 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import vercel from "@astrojs/vercel/serverless";
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import icons from 'unplugin-icons/vite'
+import AutoImport from 'unplugin-auto-import/astro'
+import IconsResolver from 'unplugin-icons/resolver'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: vercel()
-});
+  integrations: [mdx(), sitemap(), tailwind(), AutoImport({
+    dts: 'src/auto-import.d.ts',
+    resolvers: [
+      IconsResolver({
+        prefix: 'icon',
+        extension: 'jsx',
+      }),
+    ],
+  })],
+  output: 'server',
+  adapter: vercel(),
+  vite: {
+    plugins: [
+      icons({
+        compiler: 'astro',
+      }),
+    ],
+  },
+})
