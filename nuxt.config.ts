43af4e0dc12bae6ff6fb53467ba8c72ac2bc3702
gnip.gnos <abyss76647464@gmai.com>
2023-10-07 23:36:01 +0800

    :package: add unocss and daisyui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,27 @@
+import {presetUno, presetIcons} from "unocss";
+import {presetDaisy} from "unocss-preset-daisy";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: {enabled: true},
   modules: [
     '@nuxt/content',
+    "@unocss/nuxt",
   ],
-  content: {}
+  css: ["@unocss/reset/tailwind.css"],
+  content: {},
+  unocss: {
+    presets: [
+      presetUno(),
+      presetDaisy({
+        themes: false
+      }),
+      presetIcons({
+        extraProperties: {
+          "display": "inline-block",
+          "vertical-align": "middle"
+        }
+      })
+    ]
+  }
 })
