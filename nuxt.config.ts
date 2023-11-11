407682905c36762f0d16202de0fb9f411396f5d9
ping.song <pings7664@gmail.com>
2023-11-11 21:09:15 +0800

    :tada: rebase new project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,4 @@
-import {presetUno, presetIcons} from "unocss";
-import {presetDaisy} from "unocss-preset-daisy";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {enabled: true},
-  modules: [
-    '@nuxt/content',
-    "@unocss/nuxt",
-  ],
-  css: ["@unocss/reset/tailwind.css"],
-  content: {},
-  unocss: {
-    presets: [
-      presetUno(),
-      presetDaisy({
-        themes: false
-      }),
-      presetIcons({
-        extraProperties: {
-          "display": "inline-block",
-          "vertical-align": "middle"
-        }
-      })
-    ]
-  }
+  devtools: { enabled: true }
 })
