3ee29ef3327e685930fe7247c28928a548044f44
ping.song <pings7664@gmail.com>
2024-03-09 23:47:47 +0800

    🎉 重新开始项目

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {
-    enabled: true,
-
-    timeline: {
-      enabled: true,
-    },
-  },
-
-  modules: [
-    '@nuxt/content',
-    '@nuxtjs/tailwindcss',
-    'shadcn-nuxt',
-  ],
-  shadcn: {
-    prefix: '',
-    componentDir: './components/ui',
-  },
+  devtools: { enabled: true }
 })
