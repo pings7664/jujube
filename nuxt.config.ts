885de69a2a6421f564a7f8e6cdf2cd4ab8770b37
ping.song <pings7664@gmail.com>
2024-03-16 21:12:42 +0800

    ✨ use components -> shadcn-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxthq/studio'],
+  modules: ['@nuxt/content', '@nuxthq/studio', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  }
 })
