29a07c12945fdc10447fb331cb20dc07c55acc70
ping.song <pings7664@gmail.com>
2024-03-23 22:05:56 +0800

    💄 update dark mode button

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -1,36 +1,25 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
 import { Button } from '@/components/ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 
 const colorMode = useColorMode()
+
+function toggleColorMode() {
+  colorMode.preference = colorMode.value === 'light' ? 'dark' : 'light'
+}
 </script>
 
 <template>
-  <DropdownMenu>
-    <DropdownMenuTrigger as-child>
-      <Button variant="outline">
-        <Icon
-          icon="radix-icons:moon"
-          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-        />
-        <Icon
-          icon="radix-icons:sun"
-          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-        />
-        <span class="sr-only">Toggle theme</span>
-      </Button>
-    </DropdownMenuTrigger>
-    <DropdownMenuContent align="end">
-      <DropdownMenuItem @click="colorMode.preference = 'light'">
-        Light
-      </DropdownMenuItem>
-      <DropdownMenuItem @click="colorMode.preference = 'dark'">
-        Dark
-      </DropdownMenuItem>
-      <DropdownMenuItem @click="colorMode.preference = 'system'">
-        System
-      </DropdownMenuItem>
-    </DropdownMenuContent>
-  </DropdownMenu>
+  <Button
+    class="w-9 h-9"
+    aria-label="Toggle dark mode"
+    variant="ghost"
+    size="icon"
+    @click="toggleColorMode"
+  >
+    <Icon
+      :icon="colorMode.preference === 'dark' ? 'radix-icons:sun' : 'radix-icons:moon'"
+      class="w-5 h-5 text-foreground"
+    />
+  </Button>
 </template>
